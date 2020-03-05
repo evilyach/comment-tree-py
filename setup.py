1186c65d16e3a943eba3be1ac68fa3ba5b2a324d
Ilya Chesalin <ilya.chesalin@solidwall.io>
2020-03-05 21:52:32 +0300

    Added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -19,4 +19,23 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    install_requires=[
+        'astroid==2.3.3',
+        'autopep8==1.5',
+        'certifi==2019.11.28',
+        'chardet==3.0.4',
+        'gevent==1.4.0',
+        'greenlet==0.4.15',
+        'grequests==0.4.0',
+        'idna==2.8',
+        'isort==4.3.21',
+        'lazy-object-proxy==1.4.3',
+        'mccabe==0.6.1',
+        'pycodestyle==2.5.0',
+        'pylint==2.4.4',
+        'requests==2.22.0',
+        'six==1.14.0',
+        'urllib3==1.25.8',
+        'wrapt==1.11.2',
+    ]
 )
