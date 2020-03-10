d09d4c46ada5db11dc099d1e89cc79c97734ccd6
Ilya Chesalin <ilya.chesalin@solidwall.io>
2020-03-10 12:31:43 +0300

    Reorganized setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,8 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name="comment-tree-py",
     version="0.0.1",
-    author="Ilya Chesalin",
-    author_email="evilyach@protonmail.com",
-    description="Comment Tree Py CLI app to add comments to a JSON concurrently using jsonplaceholder.typicode.com API",
-    long_description=desc,
-    long_description_content_type="text/markdown",
-    url="https://github.com/evilyach/comment-tree-py",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+
     python_requires=">=3.7",
     install_requires=[
         'astroid==2.3.3',
@@ -37,5 +27,17 @@ setuptools.setup(
         'six==1.14.0',
         'urllib3==1.25.8',
         'wrapt==1.11.2',
-    ]
+    ],
+
+    author="Ilya Chesalin",
+    author_email="evilyach@protonmail.com",
+    description="Comment Tree Py CLI app to add comments to a JSON concurrently using jsonplaceholder.typicode.com API",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/evilyach/comment-tree-py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
